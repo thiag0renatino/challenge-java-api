e810d5e50b6da9ba0d3842b0e19444c321b8086e
Thiago <thiagorenatino@gmail.com>
2025-05-02 02:40:54 -0300

    primeiro commit

---
+++ src/main/java/com/fiap/challenge_api/ChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.challenge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeApiApplication.class, args);
+	}
+
+}
