44cef700303a5073d9e091cf195dbb26e594aa5e
Thiago <thiagorenatino@gmail.com>
2025-05-08 17:19:49 -0300

    feat(model): adiciona classe Usuario

---
+++ src/main/java/com/fiap/challenge_api/model/Usuario.java
@@ -0,0 +1,27 @@
+package com.fiap.challenge_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Usuario {
+
+    @Id
+    private Long idUsuario;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    private String senha;
+
+    private String status = "ativo";
+
+    @ManyToOne
+    @JoinColumn(name = "patio_id_patio")
+    private Patio patio;
+}
