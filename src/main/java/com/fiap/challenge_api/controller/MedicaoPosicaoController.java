2206f4314d73a60895bd00c5add95728ec1354ed
Thiago <thiagorenatino@gmail.com>
2025-05-09 16:42:24 -0300

    fix(controller): corrige retorno da requisição de insert

--- src/main/java/com/fiap/challenge_api/controller/MedicaoPosicaoController.java
+++ src/main/java/com/fiap/challenge_api/controller/MedicaoPosicaoController.java
@@ -4,6 +4,7 @@ import com.fiap.challenge_api.dto.MedicaoPosicaoDTO;
 import com.fiap.challenge_api.service.MedicaoPosicaoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,7 +46,7 @@ public class MedicaoPosicaoController {
 
     @PostMapping
     public ResponseEntity<MedicaoPosicaoDTO> insert(@RequestBody @Valid MedicaoPosicaoDTO dto){
-        return ResponseEntity.ok(service.insert(dto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.insert(dto));
     }
 
     @DeleteMapping("/{id}")
